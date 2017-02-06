1cbe4fe8c63cfa37cbdbb9c0485f328afc61fede
Fabio Massaioli <fabio.massaioli@gmail.com>
2017-02-06 02:01:00 +0100

    Use cmake v3.1 on travis

--- install_deps.sh
+++ install_deps.sh
@@ -7,7 +7,7 @@ set -e
 mkdir -p "$HOME/deps"
 
 if [ ! -d "$HOME/deps/cmake/bin" ]; then
-  wget --no-check-certificate -O cmake.tar.gz "https://cmake.org/files/v3.4/cmake-3.4.0-Linux-x86_64.tar.gz"
+  wget --no-check-certificate -O cmake.tar.gz "https://cmake.org/files/v3.1/cmake-3.1.0-Linux-x86_64.tar.gz"
   tar -xf cmake.tar.gz
   mv cmake-* "$HOME/deps/cmake"
   rm -rf cmake.tar.gz cmake-*
